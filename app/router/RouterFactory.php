36acdb1f08099702b0402f4742be4c24b3256e24
Patrik Votocek <patrik@votocek.cz>
2014-11-23 23:34:15 +0100

    Option to enable HTTPS

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -7,18 +7,32 @@ use Nette\Application\Routers\Route;
 
 class RouterFactory
 {
+
+	/** @var boolean */
+	private $https;
+
+	/**
+	 * @param boolean
+	 */
+	public function __construct($https = false)
+	{
+		$this->https = $https;
+	}
+
 	/**
 	 * @return \Nette\Application\IRouter
 	 */
 	public function createRouter()
 	{
+		$defaultFlags = $this->https ? Route::SECURED : 0;
+
 		$router = new RouteList();
-		$router[] = new Route('', 'Article:default');
-		$router[] = new Route('admin/login', 'Login:default');
-		$router[] = new Route('admin[/<action>]', 'Admin:Article:default');
-		$router[] = new Route('feed.xml', 'Rss:default');
-		$router[] = new Route('<slug>/feed.xml', 'Rss:comments');
-		$router[] = new Route('<slug>', 'Article:detail');
+		$router[] = new Route('', 'Article:default', $defaultFlags);
+		$router[] = new Route('admin/login', 'Login:default', $defaultFlags);
+		$router[] = new Route('admin[/<action>]', 'Admin:Article:default', $defaultFlags);
+		$router[] = new Route('feed.xml', 'Rss:default', $defaultFlags);
+		$router[] = new Route('<slug>/feed.xml', 'Rss:comments', $defaultFlags);
+		$router[] = new Route('<slug>', 'Article:detail', $defaultFlags);
 		return $router;
 	}
 }
